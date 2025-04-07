3af3c07280ac41cf61972e333a38a926b144dc5a
shoya fujisawa <6150fs@gmail.com>
2025-04-07 21:05:55 +0900

    作業詳細テーブルの作業タイプカラムの名前を修正

--- db/migrate/20250407114305_create_task_details.rb
+++ db/migrate/20250407114305_create_task_details.rb
@@ -2,7 +2,7 @@ class CreateTaskDetails < ActiveRecord::Migration[7.1]
   def change
     create_table :task_details, id: :uuid do |t|
       t.references :task, null: false, foreign_key: true, type: :uuid
-      t.integer :type, null: false
+      t.integer :task_type, null: false
       t.integer :set_count, null: false
       t.timestamp :started_at, null: false
       t.timestamp :ended_at, null: false
